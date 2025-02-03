d306dc30590259e8a2c3fff5709e94f105a6bff3
Bence Szabo <bence.szabo@arsana.io>
2025-02-03 19:26:17 +0100

    add edu details, fix h2

--- src/routes/Education.svelte
+++ src/routes/Education.svelte
@@ -13,16 +13,33 @@
     time="2014 - 2021"
     slot="headerSlot"
   />
-  <p>Juhu</p>
-</AccordionItem>
+  <ul>
+    <li>Biochemistry competition: 5th place (2015)</li>
+    <li>Biophysics competition: 6th place (2015)</li>
+    <li>Research in LinkGroup</li>
+    <li>Thesis:</li>
+  </ul></AccordionItem
+>
 
 <!-- Physics B.Sc. -->
 <AccordionItem useStyle={false}>
   <HistoryElement
     title="Physics B.Sc."
     location="Eötvös Loránd University, Budapest"
-    time="2011 - 2014"
+    time="2016 - 2020"
     slot="headerSlot"
   />
-  <p>Juhu</p>
+  <ul>
+    <li>Biophysics Specialization</li>
+    <li>Thesis:</li>
+  </ul>
 </AccordionItem>
+
+<!-- <AccordionItem useStyle={false}>
+  <HistoryElement
+    title="Math and Informatics Specialization"
+    location="Fazekas Mihály High School, Debrecen"
+    time="2008 - 2014"
+    slot="headerSlot"
+  />
+</AccordionItem> -->
