7f00828647f2a3abb58b9e3eda00520412319b60
Bence Szabo <bence.szabo@arsana.io>
2025-02-03 20:44:00 +0100

    add icons to contact

--- src/routes/Contact.svelte
+++ src/routes/Contact.svelte
@@ -1,15 +1,32 @@
 <script>
   import AccordionItem from "$lib/components/AccordionItem.svelte";
+  import { Icon } from "svelte-icons-pack";
+  import { FiMail } from "svelte-icons-pack/fi";
+  import {
+    IoLocationOutline,
+    IoLogoLinkedin,
+    IoPhonePortraitOutline,
+  } from "svelte-icons-pack/io";
+  const iconSize = 16;
 </script>
 
 <AccordionItem>
   <!-- Header Slot -->
   <p slot="headerSlot" class="accordion-header.level-1">Contact Information</p>
   <!-- Body Content -->
-
-  <ul>
-    <li>Email: bszabo96@gmail.com</li>
-    <li>Phone: +36 20 566 6348</li>
-    <li>Location: Budapest, Hungary</li>
-  </ul>
+  <div>
+    <Icon src={FiMail} color="#3178C6" size={iconSize} />
+    <a href="mailto:bszabo96@gmail.com">bszabo96@gmail.com</a>
+  </div>
+  <div>
+    <Icon src={IoPhonePortraitOutline} color="#3178C6" size={iconSize} />
+    +36 20 566 6348
+  </div>
+  <div>
+    <Icon src={IoLogoLinkedin} color="#3178C6" size={iconSize} />
+    <a href="https://www.linkedin.com/in/szabob/" target="_blank"> LinkedIn </a>
+  </div>
+  <div>
+    <Icon src={IoLocationOutline} color="#3178C6" size={iconSize} /> Budapest, Hungary
+  </div>
 </AccordionItem>
