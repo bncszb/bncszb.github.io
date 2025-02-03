8c101e2ddbec3d9fa1ed040ea6e5a38c13651ee5
Bence Szabo <bence.szabo@arsana.io>
2025-02-03 20:20:38 +0100

    add skills

--- src/routes/Skills.svelte
+++ src/routes/Skills.svelte
@@ -1,5 +1,21 @@
 <script>
   import AccordionItem from "$lib/components/AccordionItem.svelte";
+  import {
+    SiDocker,
+    SiGit,
+    SiGo,
+    SiKubernetes,
+    SiLangchain,
+    SiMlflow,
+    SiMysql,
+    SiNeo4j,
+    SiPython,
+    SiPytorch,
+    SiTensorflow,
+    SiTypescript,
+  } from "@icons-pack/svelte-simple-icons";
+
+  const iconSize = 16;
 </script>
 
 <AccordionItem isOpen={true}>
@@ -11,29 +27,46 @@
     <li>
       Programming Languages:
       <ul>
-        <li>Python</li>
-        <li>Typescript</li>
-        <li>Go</li>
+        <li>Python <SiPython color="#3776AB" size={iconSize} /></li>
+        <li>Typescript <SiTypescript color="#007ACC" size={iconSize} /></li>
+        <li>Go<SiGo color="#00ADD8" size={iconSize} /></li>
       </ul>
     </li>
     <li>
       Frameworks:
       <ul>
-        <li>PyTorch</li>
-        <li>Tensorflow</li>
-        <li>Langchain</li>
+        <li>PyTorch <SiPytorch color="#EE4C2C" size={iconSize} /></li>
+        <li>Tensorflow <SiTensorflow color="#FF6F00" size={iconSize} /></li>
+        <li>Langchain <SiLangchain color="#FF6F00" size={iconSize} /></li>
       </ul>
     </li>
     <li>
       Tools:
       <ul>
-        <li>Git</li>
-        <li>Docker</li>
-        <li>Kubernetes</li>
-        <li>MLFlow</li>
+        <li>Git <SiGit color="#F05032" size={iconSize} /></li>
+        <li>Docker <SiDocker color="#2496ED" size={iconSize} /></li>
+        <li>Kubernetes <SiKubernetes color="#326CE5" size={iconSize} /></li>
+        <li>MLFlow <SiMlflow color="#02569B" size={iconSize} /></li>
         <li>Tensorboard</li>
         <li>LangFuse</li>
       </ul>
     </li>
-  </ul>
-</AccordionItem>
+    <li>
+      Databases:
+      <ul>
+        <li>MySQL <SiMysql color="#4479A1" size={iconSize} /></li>
+        <li>Qdrant</li>
+        <li>Neo4j <SiNeo4j color="#008CC1" size={iconSize} /></li>
+      </ul>
+    </li>
+    <li>
+      Medical Ontologies:
+      <ul>
+        <li>ICD-10</li>
+        <li>DrugBank</li>
+        <li>SNOMED CT</li>
+        <li>LOINC</li>
+      </ul>
+    </li>
+  </ul></AccordionItem
+>
