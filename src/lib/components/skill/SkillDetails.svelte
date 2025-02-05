bc0fceccd1c32edf02cd1caf314f1714f3ae7801
Bence Szabo <bence.szabo@arsana.io>
2025-02-05 10:08:25 +0100

    create skill details component

---
+++ src/lib/components/skill/SkillDetails.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import type { Skill } from "$lib/models/skills";
+
+  export let skill: Skill;
+</script>
+
+<h1>{skill.name}</h1>
+<p>{skill.mastery}</p>
--- src/routes/skills/[skill]/+page.svelte
+++ src/routes/skills/[skill]/+page.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
+  import SkillDetails from "$lib/components/skill/SkillDetails.svelte";
   import type { PageProps } from "./$types";
 
   let { data }: PageProps = $props();
   let { skill } = data;
 </script>
 
-<h1>{skill.name}</h1>
-<p>{skill.mastery}</p>
+<SkillDetails {skill} />
