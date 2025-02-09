3481b27bc480f73b3302ccce8d77e39b4dc2ae3e
Bence Szabo <bence.szabo@arsana.io>
2025-02-09 18:20:53 +0100

    mermaid example

---
+++ src/routes/mermaid/+page.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+  import mermaid from "mermaid";
+  import { onMount } from "svelte";
+
+  type MermaidStore = {
+    component: any;
+  };
+  mermaid.initialize({
+    startOnLoad: false,
+  });
+
+  let mermaidChart = $state<MermaidStore>({
+    component: null,
+  });
+
+  interface PieChartData {
+    title: string;
+    items: Record<string, number>;
+  }
+
+  let pieChartData: PieChartData = {
+    title: "Pets adopted by volunteers",
+    items: {
+      Dogs: 386,
+      Cats: 85,
+      HoneyBadgers: 151,
+      Rats: 10,
+    },
+  };
+
+  const generatePieChart = (data: PieChartData): void => {
+    mermaid.run();
+    const diagram = `
+%%{init: {'theme':'base'}}%%
+pie title ${data.title}
+  ${Object.entries(data.items)
+    .map(([key, value]) => `"${key}": ${value}`)
+    .join("\n")}
+    `;
+    console.log(diagram);
+
+    mermaid.render(`pets-pie-chart`, diagram).then((component) => {
+      console.log("Chart rendered");
+      console.log(component);
+      mermaidChart.component = component.svg;
+    });
+  };
+
+  onMount(async () => {
+    generatePieChart(pieChartData);
+  });
+</script>
+
+<article>
+  <h1>Pie Chart Example</h1>
+  {#if mermaidChart.component}
+    {@html mermaidChart.component}
+  {:else}
+    <p>Loading...</p>
+  {/if}
+</article>
