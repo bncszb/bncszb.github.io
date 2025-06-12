2c67653590781472d81d21338fc7ec2621f512d4
Bence Szabó <bszabo96@gmail.com>
2025-06-12 11:40:15 +0200

    change snomed project's short description

--- src/lib/models/side-projects.ts
+++ src/lib/models/side-projects.ts
@@ -3,7 +3,8 @@ import { dockerSkill, neo4jSkill, pythonSkill, snomedSkill } from "./skills";
 
 const snomedNeo4jProject: Project = {
   name: "SNOMED Neo4j",
-  shortDescription: "Neo4j representation of SNOMED CT",
+  shortDescription:
+    "Containerized Neo4j representation of SNOMED CT with Python client",
   description: `
 A containerized SNOMED CT database using Neo4j, with an option to reduce the graph to hierarchies under certain concepts selected by the user or keep only given relationship types for a smaller image.
 
