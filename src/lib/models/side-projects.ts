968eeba52249a0f5120465ad062b15c33a1cad72
Bence Szabó <bszabo96@gmail.com>
2026-01-04 10:43:05 +0100

    add awesome medical knowledge to sideprojects

--- src/lib/models/side-projects.ts
+++ src/lib/models/side-projects.ts
@@ -1,6 +1,20 @@
 import type { Project } from "./project";
 import { dockerSkill, neo4jSkill, pythonSkill, snomedSkill } from "./skills";
 
+const awesomeMedicalKnowledgeProject: Project = {
+  name: "Awesome Medical Knowledge",
+  shortDescription:
+    "A curated list of interesting and important resources in the field of Medical Knowledge Modeling",
+  description: `
+This curated list is for me and others interested in the field of Medical Knowledge Modeling. 
+
+It collects resources that are useful for understanding medical information in a structured and explainable way. 
+It has everything from integration using FHIR, CDA or other standards to ingesting that information as knowledge graphs or RAG systems.
+`,
+  skills: [],
+  github: "https://github.com/bncszb/awesome-medical-knowledge",
+};
+
 const snomedNeo4jProject: Project = {
   name: "SNOMED Neo4j",
   shortDescription:
@@ -14,4 +28,7 @@ Due to licensing, the data is imported during local build using a UMLS API key.
   github: "https://github.com/bncszb/snomed-neo4j",
 };
 
-export const sideProjects = [snomedNeo4jProject];
+export const sideProjects = [
+  awesomeMedicalKnowledgeProject,
+  snomedNeo4jProject,
+];
