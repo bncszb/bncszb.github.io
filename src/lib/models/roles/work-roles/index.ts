cedb80b4cffb4b9cd3a7967a206bf603b9fbcf26
Bence Szabó <bszabo96@gmail.com>
2026-03-31 19:39:59 +0200

    .

--- src/lib/models/roles/work-roles/index.ts
+++ src/lib/models/roles/work-roles/index.ts
@@ -1,12 +1,12 @@
 import { biotHeadOfAI } from "$lib/models/roles/work-roles/biot-head-of-ai";
 import { biotSoftwareDeveloper } from "$lib/models/roles/work-roles/biot-software-developer";
 import { telekomSeniorAIEngineer, telekomTechLead } from "$lib/models/roles/work-roles/scaler";
-import { ulyssysAIResearcherRole } from "$lib/models/roles/work-roles/ulyssys";
+import { ulyssysAIResearcher } from "$lib/models/roles/work-roles/ulyssys";
 
 export default [
   telekomTechLead,
   telekomSeniorAIEngineer,
   biotHeadOfAI,
   biotSoftwareDeveloper,
-  ulyssysAIResearcherRole,
+  ulyssysAIResearcher,
 ];
